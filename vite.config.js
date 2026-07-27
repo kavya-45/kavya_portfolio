a9415d516027f0628ecf574c38250ec6d5fc58f6
Kavya-NK <88713660+kavya-45@users.noreply.github.com>
2026-07-27 09:54:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // 👈 this is required for GitHub Pages
+  base: '/kavya_portfolio/', // 👈 this is required for GitHub Pages
 })
